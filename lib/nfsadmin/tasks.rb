8991698bf34d702f818c9b9bee580ec9ff5b26ea
Bastiaan Schaap <b.schaap@siteminds.nl>
2014-04-07 16:51:59 +0200

    Reworking tasks

--- lib/nfsadmin/tasks.rb
+++ lib/nfsadmin/tasks.rb
@@ -1,43 +1,44 @@
 module Nfsadmin
 
-	class Tasks
-		def self.list_shares
-			begin
-				IO.popen(["/usr/sbin/exportfs -v"], ) do |io|
-				  result = io.read
-				end
-			rescue
-				puts 'Error: nfsadmin requires the exportfs command to be present.'
-			end
-		end
+  class Tasks
+    def self.get_shares
+      IO.popen(["exportfs", "-v"], ) do |io|
+        return io.readlines
+      end
+    end
 
-		def self.create_share
-			puts 'Create a new NFS share'
-		end
+    def self.list_shares
+      exports = get_shares
+      puts "Found #{exports.size} shares"
+    end
 
-		def self.delete_share
-			puts 'Delete a NFS share'
-		end
+    def self.create_share
+      puts 'Create a new NFS share'
+    end
 
-		def self.show_status
-			puts 'Show the status of the NFS service(s)'
-		end
+    def self.delete_share
+      puts 'Delete a NFS share'
+    end
 
-		def self.start_service
-			puts 'Start the NFS service(s)'
-		end
+    def self.show_status
+      puts 'Show the status of the NFS service(s)'
+    end
 
-		def self.stop_service
-			puts 'Stop the NFS service(s)'
-		end
+    def self.start_service
+      puts 'Start the NFS service(s)'
+    end
 
-		def self.restart_service
-			puts 'Restart the NFS service(s)'
-		end
+    def self.stop_service
+      puts 'Stop the NFS service(s)'
+    end
 
-		def self.reload_config
-			puts 'Reload the exported NFS shares'
-		end
-	end
+    def self.restart_service
+      puts 'Restart the NFS service(s)'
+    end
+
+    def self.reload_config
+      puts 'Reload the exported NFS shares'
+    end
+  end
 
 end
