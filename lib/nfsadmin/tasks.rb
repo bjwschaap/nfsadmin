d9e5aa8ca9bace1f262590d2112cb886d2d641e8
Bastiaan Schaap <b.schaap@siteminds.nl>
2014-04-07 15:23:52 +0200

    More tasks implemented

--- lib/nfsadmin/tasks.rb
+++ lib/nfsadmin/tasks.rb
@@ -1,8 +1,39 @@
 module Nfsadmin
 
 	class Tasks
-		def list_shares
-			puts 'List all shares'
+		def self.list_shares
+			IO.popen(["/usr/sbin/exportfs", "-v"], :err=>[:child, :out]) do |io|
+			  output = io.read
+			  puts output
+			end
+		end
+
+		def self.create_share
+			puts 'Create a new NFS share'
+		end
+
+		def self.delete_share
+			puts 'Delete a NFS share'
+		end
+
+		def self.show_status
+			puts 'Show the status of the NFS service(s)'
+		end
+
+		def self.start_service
+			puts 'Start the NFS service(s)'
+		end
+
+		def self.stop_service
+			puts 'Stop the NFS service(s)'
+		end
+
+		def self.restart_service
+			puts 'Restart the NFS service(s)'
+		end
+
+		def self.reload_config
+			puts 'Reload the exported NFS shares'
 		end
 	end
 
